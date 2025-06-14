f49b7b47f321b809f2b7cfa5cd285a9ce608a2df
mycrush2017 <info@negecace.com>
2025-06-14 13:01:42 -0400

    Add .next/static/chunks/src_app_analytics_page_tsx_a1f9e366._.js 1749920502294

---
+++ .next/static/chunks/src_app_analytics_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_53956779._.js",
+  "static/chunks/node_modules_0dd0f09b._.js"
+],
+    source: "dynamic"
+});
